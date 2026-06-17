a251e2ea760830a86dca643c1e6b3e8c504e2c79
akucintaayambali <daffaabdiantoro@gmail.com>
2026-06-17 23:34:49 +0700

    Tugas Capstone 10 Web Service Client

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,3 +30,7 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
 
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
 $routes->get('/profile', 'Profile::index', ['filter' => 'auth']);
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->get('ajax/destinations','TransaksiController::destinations', ['filter' => 'auth']);
+$routes->get('ajax/costs','TransaksiController::costs', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
